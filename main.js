23ec29e89c551d4ba4752d4d1e93a2c3c6a6f8f5
nocriteriaforaevaaaaa <aevaacharya9@gmail.com>
2023-12-28 21:58:28 +0545

    Changes

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let lastPaintTime=0;
 let snakeArr=[
     {x:13,y:15}
 ]
+food={x:6,y:7};
 
 function main(ctime){
     window.requestAnimationFrame(main);
@@ -19,18 +20,39 @@ function main(ctime){
     gameEngine();
 }
 function gameEngine(){
+    
+    //snake display
     bg.innerHTML= "";
     snakeArr.forEach((e,index)=>{
         snakeElement=document.createElement('div');
         snakeElement.style.gridRowStart=e.y;
         snakeElement.style.gridColumnStart=e.x;
-        snakeElement.classList.add('food');
+        snakeElement.classList.add('snake');
+        if (index===0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
         bg.appendChild(snakeElement);
 
       
-    })
+    });
+    
+    //food display
+    foodElement=document.createElement('div');
+        foodElement.style.gridRowStart=food.y;
+        foodElement.style.gridColumnStart=food.x;
+        foodElement.classList.add('food');
+        bg.appendChild(foodElement);
+
      
 }
+
+
+
+window.requestAnimationFrame(main);
+window.addEventListener() 
 
 
 
